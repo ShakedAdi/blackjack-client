558b9438ad8351e3000a1c0a50915336b1db8ebf
ShakedAdi <shakedadi2512@gmail.com>
2026-07-08 14:34:40 +0300

    add a call to the servers /status endpoint, and displays if the server is up or down to test communication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
+interface Status {
+  isRunning: boolean;
+}
+
 function App() {
-  return <h1>Test</h1>
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function fetchServerStatus() {
+      const response = await fetch("http://localhost:42069/status/");
+      const status: Status = await response.json();
+      setIsRunning(status.isRunning);
+    }
+
+    fetchServerStatus();
+  }, []);
+
+  return (
+    <>
+      <h1>Test</h1>
+      <h2>Server Status: {isRunning ? "UP" : "DOWN"}</h2>
+    </>
+  );
 }
 
 export default App
