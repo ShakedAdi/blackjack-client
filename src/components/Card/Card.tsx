4ff9a1a15f788a61039fe8f607a084753918a57b
ShakedAdi <shakedadi2512@gmail.com>
2026-07-18 17:19:31 +0300

    feat: add Card and CardBack components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,14 @@
 import type { Rank, Suit } from "../../types";
+import styles from './Card.module.css'
 
 interface CardProps {
   rank: Rank;
   suit: Suit;
 }
 export function Card({ rank, suit }: CardProps) {
-    return <></>;
+    return (
+        <div className={styles.cardContainer}>
+            <img className={styles.cardImg} src={`${rank.toLowerCase()}-${suit}.png`} alt={`${rank} of ${suit}`}/>
+        </div>
+    );
 }
---
+++ src/components/Card/CardBack.tsx
@@ -0,0 +1,5 @@
+import styles from './Card.module.css'
+
+export function CardBack() {
+    return <img className={styles.cardBack} src="card-back.png" alt="Card back"/>;
+}
