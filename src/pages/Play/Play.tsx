d4662b2f415b7c59e497c4ba63a3af084af38ab6
ShakedAdi <shakedadi2512@gmail.com>
2026-07-19 17:10:37 +0300

    feat: designed the layout on which the deck, dealer's hand and the player's hands are going to be placed

--- src/pages/Play/Play.tsx
+++ src/pages/Play/Play.tsx
@@ -1,55 +1,53 @@
-import { Card } from "../../components/Card/Card";
-import { CardBack } from "../../components/Card/CardBack";
+import { useState } from "react";
 import { Deck } from "../../components/Deck/Deck";
-import { Hand } from "../../components/Hand/Hand";
 import { PlayerHands } from "../../components/PlayerHands/PlayerHands";
+import { type Hand as HandType, type DealerHand as DealerHandType } from "../../types";
+import styles from "./Play.module.css";
 import { DealerHand } from "../../components/DealerHand/DealerHand";
 
 export function Play() {
+  const [dealerHand, setDealerHand] = useState<DealerHandType>({cards: [{rank: "10", suit: "spades"}, {rank: "J", suit: "spades"}], isHoleCardHidden: true});
+  const [playerHands, setPlayerHands] = useState<HandType[]>([
+    {
+      cards: [
+        {rank: "A", suit: "hearts"}, 
+        {rank: "10", suit: "clubs"}, 
+        {rank: "K", suit: "hearts"}
+      ]
+    },
+    {
+      cards: [
+        {rank: "5", suit: "hearts"}, 
+        {rank: "5", suit: "clubs"},
+      ]
+    },
+    {
+      cards: [
+        {rank: "2", suit: "hearts"}, 
+        {rank: "2", suit: "clubs"}, 
+        {rank: "2", suit: "spades"},
+        {rank: "2", suit: "diamonds"}
+      ]
+    },
+    {
+      cards: [
+        {rank: "6", suit: "clubs"}, 
+        {rank: "4", suit: "diamonds"}
+      ]
+    },
+  ]);
+  
   return (
-    <>
-      <Card rank="A" suit="hearts"/>
-      <CardBack/>
-      <Deck/>
-      <Hand cards={[{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},{rank: "A", suit: "hearts"}, {rank: "10", suit: "clubs"}, {rank: "K", suit: "hearts"},]}/>
-      <PlayerHands hands={
-        [
-          {
-            cards: [
-              {rank: "A", suit: "hearts"},
-              {rank: "10", suit: "clubs"},
-              {rank: "K", suit: "hearts"}
-            ]
-          },
-          {
-            cards: [
-              {rank: "5", suit: "hearts"},
-              {rank: "5", suit: "clubs"},
-            ]
-          },
-          {
-            cards: [
-              {rank: "2", suit: "hearts"},
-              {rank: "2", suit: "clubs"},
-              {rank: "2", suit: "spades"},
-              {rank: "2", suit: "diamonds"}
-            ]
-          },
-          {
-            cards: [
-              {rank: "6", suit: "clubs"},
-              {rank: "4", suit: "diamonds"}
-            ]
-          },
-        ]
-      }/>
-      <DealerHand
-        cards={[
-          {rank: "6", suit: "clubs"},
-          {rank: "4", suit: "diamonds"}
-        ]}
-        isHoleCardHidden={true}
-      />
-    </>
+    <div className={styles.board}>
+      <div className={styles.dealerHand}>
+        <DealerHand cards={dealerHand.cards} isHoleCardHidden={dealerHand.isHoleCardHidden}/>
+      </div>
+      <div className={styles.deck}>
+        <Deck/>
+      </div>
+      <div className={styles.playerHands}>
+        <PlayerHands hands={playerHands}/>
+      </div>
+    </div>
   );
 }
