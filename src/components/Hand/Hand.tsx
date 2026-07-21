04603991a225dff6fe183af3a42c85ede74321bd
ShakedAdi <shakedadi2512@gmail.com>
2026-07-21 12:36:27 +0300

    feat: add the hand outcome label on top of each hand when roud is over

--- src/components/Hand/Hand.tsx
+++ src/components/Hand/Hand.tsx
@@ -1,10 +1,17 @@
-import type { Hand as HandType } from '../../types';
+import { HandOutcome, type Hand as HandType } from '../../types';
 import { Card } from '../Card/Card';
 import styles from './Hand.module.css';
 
-export function Hand({ cards, bet }: HandType) {
+export function Hand({ cards, bet, outcome = undefined }: HandType) {
   return (
     <div className={styles.handContainer}>
+      {outcome === HandOutcome.Win ? (
+        <p className={styles.winLabel}>Win</p>
+      ) : outcome === HandOutcome.Loss ? (
+        <p className={styles.lossLabel}>Loss</p>
+      ) : (
+        outcome === HandOutcome.Push && <p className={styles.pushLabel}>Push</p>
+      )}
       <p className={styles.betLabel}>Bet: {bet}</p>
       <div className={styles.cardsContainer}>
         {cards.map((card) => (
